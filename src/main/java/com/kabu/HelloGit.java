62fa8e3690130fc26adcbdb278037ccf48046427
kabu <2529987705@qq.com>
2023-03-08 17:11:23 +0800

    master test

--- src/main/java/com/kabu/HelloGit.java
+++ src/main/java/com/kabu/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello git1!");
         System.out.println("hello git2!");
         System.out.println("hello git2! dev");
+        System.out.println("hello git2! master");
     }
 }
